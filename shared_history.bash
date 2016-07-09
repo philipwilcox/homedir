302d23b2d554fffd05cd0fdd352eede49fb21273
Philip Wilcox <philipwilcox@gmail.com>
2016-07-08 23:30:22 -0700

    add a file to update history across multiple terminal windows

---
+++ shared_history.bash
@@ -0,0 +1,8 @@
+# From http://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows
+export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+export HISTSIZE=100000                   # big big history
+export HISTFILESIZE=100000               # big big history
+shopt -s histappend                      # append to history, don't overwrite it
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+PATH=$PATH:$HOME/bin
