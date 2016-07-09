7d86e8da5f09ca7a1d523585c3e65d280bc2ecf6
Philip Wilcox <philipwilcox@gmail.com>
2016-07-09 00:08:47 -0700

    bug fixes around evaluation time, color escaping

--- git_prompt.bash
+++ git_prompt.bash
@@ -1,4 +1,13 @@
+function make_git_string() {
+    local DEFAULT="\033[0m"
+    local LIGHT_MAGENTA="\033[1;35m"
+    if git branch 1>/dev/null 2>/dev/null; then
+        echo -e "$DEFAULT{on $LIGHT_MAGENTA$(git rev-parse --abbrev-ref HEAD)$DEFAULT}"
+    fi
+}
+
 function set_prompt {
+    # include differently-escaped versions of the color codes here... there's gotta be a better way :\
     local BLUE="\[\033[0;34m\]"
     local LIGHT_BLUE="\[\033[1;34m\]"
     local GREEN="\[\033[0;32m\]"
@@ -11,14 +20,6 @@ function set_prompt {
     local LIGHT_CYAN="\[\033[1;36m\]"
     local DEFAULT="\[\033[0m\]"
 
-    function make_git_string() {
-        GIT_STRING=""
-        if git branch 1>/dev/null 2>/dev/null; then
-            GIT_STRING="${DEFAULT}{on $LIGHT_MAGENTA\$(git rev-parse --abbrev-ref HEAD)$DEFAULT}"
-        fi
-        echo $GIT_STRING
-    }
-
-    PS1="\n$LIGHT_GRAY[\d \t] $DEFAULT$GREEN\u$DEFAULT@$BLUE\h:$LIGHT_GREEN\w $LIGHT_BLUE$(make_git_string)\n$LIGHT_CYAN💰  $DEFAULT"
+    PS1="\n$LIGHT_GRAY[\d \t] $DEFAULT$GREEN\u$DEFAULT@$BLUE\h:$LIGHT_GREEN\w $LIGHT_BLUE\$(make_git_string)\n$LIGHT_CYAN💰  $DEFAULT"
 }
 set_prompt
