fb879dbc62f84347e71ef2a1aefd73c0de76db6f
Philip Wilcox <philipwilcox@gmail.com>
2016-07-09 00:20:38 -0700

    add smiley/sad emoji dependent on return value

--- git_prompt.bash
+++ git_prompt.bash
@@ -6,6 +6,21 @@ function make_git_string() {
     fi
 }
 
+function return_value_status() {
+    LAST_STATUS=$?
+    # based on http://serverfault.com/a/4894 -- maybe i should standardize on color escaping with similar stuff from how
+    # that answer does it
+    local LIGHT_RED="\033[1;31m"
+    local WHITE="\033[37;1m"
+    local SMILEY="${WHITE}😊 "
+    local FROWNY="${LIGHT_RED}😣 "
+    if [ $LAST_STATUS = 0 ]; then
+        echo -e "${SMILEY}"
+    else
+        echo -e "${FROWNY}"
+    fi
+}
+
 function set_prompt {
     # include differently-escaped versions of the color codes here... there's gotta be a better way :\
     local BLUE="\[\033[0;34m\]"
@@ -20,6 +35,6 @@ function set_prompt {
     local LIGHT_CYAN="\[\033[1;36m\]"
     local DEFAULT="\[\033[0m\]"
 
-    PS1="\n$LIGHT_GRAY[\d \t] $DEFAULT$GREEN\u$DEFAULT@$BLUE\h:$LIGHT_GREEN\w $LIGHT_BLUE\$(make_git_string)\n$LIGHT_CYAN💰  $DEFAULT"
+    PS1="\n\$(return_value_status) $LIGHT_GRAY[\d \t] $DEFAULT$GREEN\u$DEFAULT@$BLUE\h:$LIGHT_GREEN\w $LIGHT_BLUE\$(make_git_string)\n$LIGHT_CYAN💰  $DEFAULT"
 }
 set_prompt
