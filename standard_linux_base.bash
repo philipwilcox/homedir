04c345fe4480228f385828a187cc3eb0d1cbfc9b
Philip Wilcox <philip@philipwilcox.com>
2016-09-02 20:11:50 -0700

    add top-level wrapper suitable for standard linux installs

---
+++ standard_linux_base.bash
@@ -0,0 +1,4 @@
+[[ -s "$HOME/Repos/homedir/git_prompt.bash" ]] && source "$HOME/Repos/homedir/git_prompt.bash"
+[[ -s "$HOME/Repos/homedir/shared_history.bash" ]] && source "$HOME/Repos/homedir/shared_history.bash"
+
+export EDITOR=emacs
