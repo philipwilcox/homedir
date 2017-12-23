7e140a427a4613c5c5cb5922b2c2eca390b77cef
Philip Wilcox <philip@philipwilcox.com>
2017-12-23 11:59:59 -0500

    Add emacs.d/init.el file for backup file settings

---
+++ standard_emacs_d_init.el
@@ -0,0 +1,4 @@
+;; backup in one place. flat, no tree structure
+;; http://ergoemacs.org/emacs/emacs_set_backup_into_a_directory.html
+(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
+
