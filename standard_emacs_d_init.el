81ef1494849501ca12ec1605dea59e101ab22f19
Philip Wilcox <philip@philipwilcox.com>
2017-12-25 07:53:27 -0500

    Add Melpa package repo for markdown-mode installation

--- standard_emacs_d_init.el
+++ standard_emacs_d_init.el
@@ -2,3 +2,10 @@
 ;; http://ergoemacs.org/emacs/emacs_set_backup_into_a_directory.html
 (setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
 
+;; setup for installing markdown-mode
+;; https://jblevins.org/projects/markdown-mode/
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-stable"
+	       . "https://stable.melpa.org/packages/"))
+(package-initialize)
