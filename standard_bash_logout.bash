71efc22c9b6045715b136f848e34c4a7dcb421cc
Philip Wilcox <philip@philipwilcox.com>
2018-02-03 20:40:52 -0500

    Add ssh-agent setup stuff

---
+++ standard_bash_logout.bash
@@ -0,0 +1,4 @@
+# Copy this to ~/.bash_logout
+
+# SSH agent cleanup
+ssh-agent -k
--- standard_linux_base.bash
+++ standard_linux_base.bash
@@ -2,3 +2,14 @@
 [[ -s "$HOME/Repos/homedir/shared_history.bash" ]] && source "$HOME/Repos/homedir/shared_history.bash"
 
 export EDITOR=emacs
+
+# Here's a discussion of using SSH agent on arch wiki, in conjunction with "AddKeysToAgent yes"
+# I slightly modified to silence output.
+# https://wiki.archlinux.org/index.php/SSH_keys#SSH_agents
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    eval "$(ssh-agent > ~/.ssh-agent-deets)" &> /dev/null
+fi
+if [[ "$SSH_AGENT_PID" == "" ]]; then
+    eval "$(<~/.ssh-agent-deets)" &> /dev/null
+fi
+# BUT NOTE THAT I NEED A CORRESPONDING `ssh-agent -k` in .bash_logout
